dadadfee1e20279978bf3ee5b3eaf65ccdc6b9ce
Ariel <129072087+0xARCOS@users.noreply.github.com>
2025-06-14 05:38:42 +0200

    fix: conform stack_utils.c to Norminette

--- src/algorithm/stack_utils.c
+++ src/algorithm/stack_utils.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codex <codex@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 00:00:00 by codex             #+#    #+#             */
+/*   Updated: 2024/06/06 00:00:00 by codex            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int stack_size(t_node *stack)
+int	stack_size(t_node *stack)
 {
-    int size;
+	int		size;
 
-    size = 0;
-    while (stack)
-    {
-        size++;
-        stack = stack->next;
-    }
-    return size;
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
 }
