90ba926782969bc514a66120f1f46c7abc12fa58
Ariel <129072087+0xARCOS@users.noreply.github.com>
2025-06-14 22:24:38 +0200

    Refactor main.c for norm compliance

--- src/main.c
+++ src/main.c
@@ -1,62 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codex <codex@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 00:00:00 by codex             #+#    #+#             */
+/*   Updated: 2024/06/06 00:00:00 by codex            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <unistd.h>
-
-static int has_only_spaces(const char *s)
+static int	has_only_spaces(const char *s)
 {
-    int i;
+	int	i;
 
-    if (!s)
-        return (1);
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] != ' ' && s[i] != '\t')
-            return (0);
-        i++;
-    }
-    return (1);
+	if (!s)
+		return (1);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] != ' ' && s[i] != '	')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-int main(int argc, char **argv)
+static int	validate_args(int argc, char **argv)
 {
-    t_node      *stack_a;
-     t_node      *stack_b;
-    
-    stack_a = NULL;
-     stack_b = NULL;
+	int	i;
 
-    if (argc < 2)
-        return (0); // no hay números a ordenar
+	i = 1;
+	while (i < argc)
+	{
+		if (has_only_spaces(argv[i]))
+		{
+			write(2, "Error\n", 6);
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
 
-    int i = 1;
-    while (i < argc)
-    {
-        if (has_only_spaces(argv[i]))
-        {
-            write(2, "Error\n", 6);
-            return 1;
-        }
-        i++;
-    }
+int	main(int argc, char **argv)
+{
+	t_node	*stack_a;
+	t_node	*stack_b;
 
-    stack_a = parse_input(argc, argv);
-    if (!stack_a)
-    {
-        write(2, "Error\n", 6);
-        return 1; // Error de parsing
-    }
-
-    // Verificamos si está ordenado
-    if (is_sorted(stack_a))
-    {
-        free_stack(&stack_a);
-        return (0); // Nada que hacer
-    }
-
-    sort_stack(&stack_a, &stack_b);
-
-    free_stack(&stack_a);
-    free_stack(&stack_b);
-
-    return 0;
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc < 2)
+		return (0);
+	if (!validate_args(argc, argv))
+		return (1);
+	stack_a = parse_input(argc, argv);
+	if (!stack_a)
+		return (write(2, "Error\n", 6));
+	if (is_sorted(stack_a))
+	{
+		free_stack(&stack_a);
+		return (0);
+	}
+	sort_stack(&stack_a, &stack_b);
+	free_stack(&stack_a);
+	free_stack(&stack_b);
+	return (0);
 }
