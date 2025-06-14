df9eebc4ed129661d12aea511b48a702be488032
Ariel <129072087+0xARCOS@users.noreply.github.com>
2025-06-14 20:16:30 +0200

    Fix norminette issues in push_ops

--- src/movements/push_ops.c
+++ src/movements/push_ops.c
@@ -1,18 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_ops.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: codex <codex@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/06 00:00:00 by codex             #+#    #+#             */
+/*   Updated: 2024/06/06 00:00:00 by codex            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 #include <unistd.h>
 
-void pa(t_node **a, t_node **b)
+void	pa(t_node **a, t_node **b)
 {
-    if (!b || !*b)
-        return;
-    push_node(a, b);
-    write(1, "pa\n", 3);
+	if (!b || !*b)
+		return ;
+	push_node(a, b);
+	write(1, "pa\n", 3);
 }
 
-void pb(t_node **a, t_node **b)
+void	pb(t_node **a, t_node **b)
 {
-    if (!a || !*a)
-        return;
-    push_node(b, a);
-    write(1, "pb\n", 3);
+	if (!a || !*a)
+		return ;
+	push_node(b, a);
+	write(1, "pb\n", 3);
 }
